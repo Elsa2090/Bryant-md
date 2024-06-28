63387bc488f6f01dd3813b54aa2d552cf19270ca
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-06-28 20:26:39 +0000

    Add files via upload

---
+++ beltah/Reboot.js
@@ -0,0 +1,31 @@
+const {zokou}=require("../framework/zokou")
+
+
+
+
+
+
+
+zokou({nomCom:"restart",categorie:"Mods",reaction:"📴"},async(dest,z,com)=>{
+
+
+  
+const{repondre,ms,dev,superUser}=com;
+
+  if(!superUser)
+  {
+    return repondre("This command is for owner only");
+  }
+
+  const {exec}=require("child_process")
+
+    repondre("*Beltah-md bot is Restarting from the saver...*");
+
+  exec("pm2 restart all");
+  
+
+  
+
+
+
+})
