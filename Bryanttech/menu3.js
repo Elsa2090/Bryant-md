985a33803dca2a5d72b6599049d15519b7ced365
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-06-29 03:44:25 +0000

    Update menu3.js

--- Bryanttech/menu3.js
+++ Bryanttech/menu3.js
@@ -32,7 +32,7 @@ const temps = moment().format('HH:mm:ss');
 const date = moment().format('DD/MM/YYYY');
 
   let infoMsg =  `
-╭────《《𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗗》》────⬡
+╭────《《𝗕𝗥𝗬𝗔𝗡𝗧 𝗠𝗗》》────⬡
 ┴  ╭─────────────⬡
 │❒⁠⁠⁠⁠│ *ADMIN* : ${s.OWNER_NAME}
 │❒│⁠⁠⁠⁠ *CALENDER* : ${date}
@@ -41,17 +41,17 @@ const date = moment().format('DD/MM/YYYY');
 │❒│⁠⁠⁠⁠ *ORDERS* : ${cm.length} 
 │❒│⁠⁠⁠⁠ *SPACE* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
 │❒│⁠⁠⁠⁠ *CHROME* : ${os.platform()}
-│❒│⁠⁠⁠⁠ *THEME* : *BELTAH KE*
+│❒│⁠⁠⁠⁠ *THEME* : *𝙱𝚁𝚈𝙰𝙽𝚃 𝚃𝙴𝙲𝙷*
 ┬  ╰──────────────⬡
-╰─── ···▸ *💰LIKE A BOSS💰* ··──⬡\n`;
+╰─── ···▸ *✦𝚁𝙴𝚂𝙿𝙴𝙲𝚃 𝚃𝙷𝙸𝚂 𝚄𝚂𝙴𝚁✦* ··──⬡\n`;
     
 let menuMsg = `
 ✇─────✇────✇ 
-✇ *𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗗 V3.0.0* ✇
+✇ *𝗕𝗥𝗬𝗔𝗡𝗧 𝗠𝗗* ✇
 ✇─────✇────✇
 
 
- *✇BELTAH-MD COMMANDS✇*
+ *✦𝗕𝗥𝗬𝗔𝗡𝗧-𝗠𝗗 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦✦*
 `;
 
     for (const cat in coms) {
@@ -70,7 +70,7 @@ let menuMsg = `
 |⏣ *RELEASED ON : 02.06.2024*
 
 |⏣THANK YOU FOR CHOOSING 
-    *𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗗*
+    *𝗕𝗥𝗬𝗔𝗡𝗧 𝗠𝗗*
 *❒⁠⁠⁠⁠—————————— ❒⁠⁠⁠⁠——————————❒⁠⁠⁠⁠*
 `;
 
@@ -78,7 +78,7 @@ let menuMsg = `
 
    if (lien.match(/\.(mp4|gif)$/i)) {
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" , gifPlayback : true }, { quoted: ms });
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Bryantmd*, déveloper Bryant Tech" , gifPlayback : true }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
@@ -88,7 +88,7 @@ let menuMsg = `
 // Vérification pour .jpeg ou .png
 else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
     try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" }, { quoted: ms });
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Bryantmd*, déveloper Bryant Tech" }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
