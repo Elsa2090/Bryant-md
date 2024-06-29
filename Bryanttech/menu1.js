044987313fca10a3a3c215352f4cc4da77b03a38
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-06-29 03:37:01 +0000

    Update menu1.js

--- Bryanttech/menu1.js
+++ Bryanttech/menu1.js
@@ -32,7 +32,7 @@ const temps = moment().format('HH:mm:ss');
 const date = moment().format('DD/MM/YYYY');
 
   let infoMsg =  `
-*BELTAH-MD AVAILABLE MENUS* 
+*BRYANT-MD AVAILABLE MENUS* 
 ╭─────────────────✣
 │❒⁠⁠⁠⁠╭─────────────✣
 │❒⁠⁠⁠⁠│▸ *MENU1* 
@@ -42,22 +42,22 @@ const date = moment().format('DD/MM/YYYY');
 │❒⁠⁠⁠⁠│▸ *PLUGINS* : ${cm.length} 
 │❒⁠⁠⁠⁠│▸ *RAM* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
 │❒⁠⁠⁠⁠│▸ *SAVER* : ${os.platform()}
-│❒⁠⁠⁠⁠│▸ *THEME* : *BELTAH KE*
+│❒⁠⁠⁠⁠│▸ *THEME* : *❣️𝙱𝚁𝚈𝙰𝙽𝚃 𝚃𝙴𝙲𝙷❣️*
 │❒⁠⁠⁠⁠╰──────────────✇
 ╰──────────────────✇\n`;
     
 let menuMsg = `
 ╭─────────❒⁠⁠⁠⁠
-   *like a boss*
+   *RESPECT THIS USER😌*
 ╰─────────❒
-   ⁠⁠⁠⁠◔͜͡◔MADE EASY BY BELTAH◔͜͡◔
+   ⁠⁠⁠⁠◔͜͡◔MADE EASY BY BRYANT TECH◔͜͡◔
 ╰───────────────────⏣`;
 
    var lien = mybotpic();
 
    if (lien.match(/\.(mp4|gif)$/i)) {
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" , gifPlayback : true }, { quoted: ms });
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Bryantmd*, déveloper Bryant Tech" , gifPlayback : true }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
@@ -67,7 +67,7 @@ let menuMsg = `
 // Vérification pour .jpeg ou .png
 else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
     try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" }, { quoted: ms });
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Bryantmd*, déveloper Bryant Tech" }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
