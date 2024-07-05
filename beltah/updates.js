6978bda2e77f563a08c5011c55c34a60949129ea
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-07-05 06:00:16 +0000

    Update updates.js

--- beltah/updates.js
+++ beltah/updates.js
@@ -26,8 +26,8 @@ console.log("mon test");
 
 zokou({ nomCom: "developer", reaction: "🌟", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = ' *HERE ARE MY OWNER INFO* \n\n✇ *NAME* : Brian Kanyiri Wainaina\n✇ *AGE* : 21 years\n✇ *SEX* : MALE\n✇ *RELIGION* : Christian\n✇ *NATIONALITY* :Kenyan 🇰🇪\n✇ *FAV TEAM* :Manchester United\n✇ *REAL LIFE* :I am a plumber, I never learned about coding.\n✇ *HOBBY* : Scrolling on websites\n✇ *CONTACT BELTAH* :👇 👇 \n';
-    let d = '𓄂 https://wa.me/254114141192\n𓄂 https://wa.me/254737681758\n\n *THANK YOU FOR LOVING BELTAH-MD*. ';
+    let z = ' *HERE ARE MY OWNER INFO* \n\n✇ *NAME* : BRYANT TECH\n✇ *AGE* : 15 years\n✇ *SEX* : MALE\n✇ *RELIGION* : Christian\n✇ *NATIONALITY* :ASK HIM\n✇ *FAV TEAM* :Manchester United\n✇ *REAL LIFE* :I am a coder, I never learned about coding.\n✇ *HOBBY* : Scrolling on websites\n✇ *CONTACT BRYANT TECH* :👇 👇 \n';
+    let d = '𓄂 https://wa.me/233520729233\n𓄂 https://wa.me/233530729233\n\n *THANK YOU FOR LOVING BRYANT-MD*. ';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
