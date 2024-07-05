49cd2dbef85d07dcf15387767db1a4151cf7877b
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-07-05 05:50:19 +0000

    Update uptime.js

--- beltah/uptime.js
+++ beltah/uptime.js
@@ -37,7 +37,7 @@ function _0x4925() {
         'tion',
         'RQOlG',
         'SdpVK',
-        'f\x20BELTAH-M',
+        'f\x20BRYANT-M',
         'tEboL',
         'oyZkt',
         'mHbij',
@@ -223,7 +223,7 @@ zokou({
     'nomCom': _0x65381a(0x101),
     'desc': _0x65381a(0x102) + _0x65381a(0xc7),
     'Categorie': _0x65381a(0xf3),
-    'reaction': '🏃‍♂️',
+    'reaction': '⚡',
     'fromMe': _0x65381a(0xc6)
 }, async (_0x1c57c2, _0x592785, _0x282835) => {
     const _0x2a662b = _0x65381a, _0x754e53 = {
