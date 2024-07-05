b193055118f363cfa955f9e86898f1df7c01cec4
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-07-05 04:57:18 +0000

    Update Madara.js

--- beltah/Madara.js
+++ beltah/Madara.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "Sharingan", reaction: "😈", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "Bryant", reaction: "😈", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝐒𝚮𝚫𝚪𝚰𝚴𝐆𝚫𝚴 𝚫𝐂𝚻𝚰𝛁𝚫𝚻𝚵𝐃 \n\n ' + "║❒ 𝐓𝐘𝐏𝐄 𝐌𝐄𝐍𝐔 𝐓𝐎 𝐒𝐄𝐄 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒";
-    let d = '                                            ║❒ 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 https://t.me/+H2CcYqSn6zFhZTM0';
+    let z = '𝑩𝑹𝒀𝑨𝑵𝑻-𝑴𝑫 𝑰𝑺 𝑨𝑪𝑻𝑰𝑽𝑬 \n\n ' + "║❒ 𝐓𝐘𝐏𝐄 𝐌𝐄𝐍𝐔 𝐓𝐎 𝐒𝐄𝐄 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒";
+    let d = '                                            ║❒ 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 https://t.me/233530729233';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/388c8f5c23b4e7662ebfb.mp4';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
