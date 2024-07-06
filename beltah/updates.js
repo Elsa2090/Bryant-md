b32f49b38473efa06cd798cd6ba2f57aefba2e1d
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-07-06 12:04:26 +0000

    Add files via upload

--- beltah/updates.js
+++ beltah/updates.js
@@ -4,10 +4,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "wagroup", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Hello 👋\n\nClick The link below to Join 𝗕𝗥𝗬𝗔𝗡𝗧 𝗠𝗗 WhatsApp Testing Group\n\n';
-    let d = 'https://chat.whatsapp.com/Bkf3xlt8RTDLJdqtvMKL4X';
+    let z = 'Hello 👋\n\nClick The link below to Join 𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗗 WhatsApp Testing Group\n\n';
+    let d = 'https://chat.whatsapp.com/CQvUnqaDK4fBjgMXvFEAsI';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/c40ec1edc5e034f59db0b.mp4';
+    var img = 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -15,10 +15,10 @@ console.log("mon test");
 
 zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Hello 👋\n\nClick The link below to Join 𝗕𝗥𝗬𝗔𝗡𝗧 𝗠𝗗 WhatsApp Channel\n\n';
-    let d = 'https://whatsapp.com/channel/0029VacpEdXIt5rqKLB9nC1L';
+    let z = 'Hello 👋\n\nClick The link below to Join 𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗗 WhatsApp Channel\n\n';
+    let d = 'https://whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/6f4be641947bfe51b8bb6.mp4';
+    var img = 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -26,10 +26,10 @@ console.log("mon test");
 
 zokou({ nomCom: "developer", reaction: "🌟", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = ' *HERE ARE MY OWNER INFO* \n\n✇ *NAME* : Brian Kanyiri Wainaina\n✇ *AGE* : 21 years\n✇ *SEX* : MALE\n✇ *RELIGION* : Christian\n✇ *NATIONALITY* :𝗖𝗢𝗨𝗡𝗧𝗥𝗬𝗡𝗜𝗔𝗡\n✇ *FAV TEAM* :Manchester United\n✇ *REAL LIFE* :𝗜 𝗔𝗠 𝗔 𝗛𝗔𝗖𝗞𝗘𝗥 𝗜𝗡 𝗥𝗘𝗔𝗟 𝗟𝗜𝗙𝗘, 𝗜 𝗡𝗘𝗩𝗘𝗥 𝗟𝗘𝗔𝗥𝗡𝗧 𝗜𝗧 𝗙𝗥𝗢𝗠 𝗔𝗡𝗬 𝗕𝗢𝗗𝗬.\n✇ *HOBBY* : Scrolling on websites\n✇ *𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗕𝗥𝗬𝗔𝗡𝗧 𝗧𝗘𝗖𝗛* :⟱⟱ \n';
-    let d = '♕︎ https://wa.me/233530729233\n༆ https://wa.me/94784192378\n\n *THANK YOU FOR LOVING 𝗕𝗥𝗬𝗔𝗡𝗧-𝗠𝗗*. ';
+    let z = ' *HERE ARE MY OWNER INFO* \n\n✇ *NAME* : Brian Kanyiri Wainaina\n✇ *AGE* : 21 years\n✇ *SEX* : MALE\n✇ *RELIGION* : Christian\n✇ *NATIONALITY* :Kenyan 🇰🇪\n✇ *FAV TEAM* :Manchester United\n✇ *REAL LIFE* :I am a plumber, I never learned about coding.\n✇ *HOBBY* : Scrolling on websites\n✇ *CONTACT BELTAH* :👇 👇 \n';
+    let d = '𓄂 https://wa.me/254114141192\n𓄂 https://wa.me/254737681758\n\n *THANK YOU FOR LOVING BELTAH-MD*. ';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/a48f30c31edaca874af4b.jpg';
+    var img = 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -37,10 +37,10 @@ console.log("mon test");
 
 zokou({ nomCom: "test", reaction: "✅", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = ' *☹︎ 𝗕𝗥𝗬𝗔𝗡𝗧-𝗠𝗗 𝗕𝗢𝗧 𝗜𝗦 𝗖𝗨𝗥𝗥𝗘𝗡𝗧𝗟𝗬 𝗔𝗖𝗧𝗜𝗩𝗘 凸(｀0´)凸*\n';
-    let d = ' ❄︎ *_MADE ON EARTH BY BRYANT TECH TEAM_* ❄︎';
+    let z = ' *☹︎ BELTAH-MD BOT IS CURRENTLY ACTIVE ☹︎*\n';
+    let d = ' ❄︎ *_MADE ON EARTH BY BELTAH TECH TEAM_* ❄︎';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/28aeb226ef2badcbbd30f.mp4';
+    var img = 'https://telegra.ph/file/bc12002382105e156a9d1.mp4';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -48,10 +48,10 @@ console.log("mon test");
 
 zokou({ nomCom: "bot", reaction: "🤖", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = ' *BRYANT-MD MAIN FEATURES* \n\n 1.Auto view status✅\n 2.Always online✅\n 3.A1 chat gpt enabled \n 4.Download songs/images✅\n 5.Open view once photos✅\n 6.Antidelete messages✅\n 7.Auto block contacts✅\n 8.Auto bluetick✅\n\n';
-    let d = ' *THANK YOU FOR LOVING BRYANT-MD*';
+    let z = ' *BELTAH-MD MAIN FEATURES* \n\n 1.Auto view status✅\n 2.Always online✅\n 3.A1 chat gpt enabled \n 4.Download songs/images✅\n 5.Open view once photos✅\n 6.Antidelete messages✅\n 7.Auto block contacts✅\n 8.Auto bluetick✅\n\n';
+    let d = ' *THANK YOU FOR LOVING BELTAH-MD*';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/6f4be641947bfe51b8bb6.mp4';
+    var img = 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -60,9 +60,9 @@ console.log("mon test");
 zokou({ nomCom: "g*y", reaction: "🏳️‍🌈", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = ' *F*CK MEHN,,, THIS GUY IS G*Y🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈*\n';
-    let d = 'WHY WOULD YOU REQUEST THAT COMMAND IF NOT (G*Y)';
+    let d = 'WHY WOULD YOU REQUEST THAT COMMAND IF NOT';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/92e7281eca9d48f39fccc.png';
+    var img = 'https://telegra.ph/file/5d4c27c77b240483e7b05.png';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -70,10 +70,10 @@ console.log("mon test");
 
 zokou({ nomCom: "check", reaction: "🗿", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = ' *BRYANT-MD UPDATES*\n\n';
+    let z = ' *BELTAH-MD UPDATES*\n\n';
     let d = 'Checking for updates...⏳\n\nUpdating new version...⏳\n\ndone....✅\n\n *𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗗 NEW VERSION SUCCESSFULLY INSTALLED...* ';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/42b9fd9b272f93b04b1d5.jpg';
+    var img = 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -81,10 +81,10 @@ console.log("mon test");
 
 zokou({ nomCom: "version", reaction: "⏳", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = ' *YOU ARE CURRENTLY USING BRYANT-MD*\n\n';
+    let z = ' *YOU ARE CURRENTLY USING BELTAH-MD V3.0.0*\n\n';
     let d = '✅latest version installed...';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/66fcd6748f3d2b02ca6e0.mp4';
+    var img = 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -95,16 +95,16 @@ zokou({ nomCom: "bugmenu", reaction: "🐛", nomFichier: __filename }, async (de
     let z = '┌─❒𝗕𝗘𝗟𝗧𝗔𝗛-𝗠𝗗 𝗕𝗨𝗚 𝗠𝗘𝗡𝗨⁠⁠⁠⁠❒⁠⁠⁠\n│╭─────────────···\n┴│▸\n⬡│▸ ʙᴜɢ\n⬡│▸ ʙᴜɢᴠ\n⬡│▸ ʙᴜɢʀᴇᴀᴄᴛ\n⬡│▸ ᴀᴜᴅɪᴏʙᴜɢ\n⬡│▸ ɪᴏꜱʙᴜɢ\n⬡│▸ ɪᴏꜱʙᴜɢᴠ\n┬│▸\n│╰────────────···▸▸\n';
     let d = '└───────────────···▸';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/3f652c6e14012f5e96434.jpg';
+    var img = 'https://telegra.ph/file/e24fe04ebb2353c3c693e.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
 console.log("mon test");
 
-zokou({ nomCom: "test", reaction: "👨‍💻", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "genz", reaction: "🇰🇪", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝗧𝗘𝗦𝗧𝗜𝗡𝗚......;
-    let d = '𝗜 𝗔𝗠 𝗕𝗥𝗛𝗔𝗡𝗧-𝗠𝗗 𝗖𝗥𝗘𝗔𝗧𝗘𝗗 𝗕𝗬 𝗕𝗥𝗬𝗔𝗡𝗧 𝗧𝗘𝗖𝗛';
+    let z = ' *❒⁠⁠⁠⁠ #REJECTFINANCEBILL2024 ❒⁠⁠⁠⁠*\n\nClick The link below to Join *❒⁠⁠⁠⁠GEN Z❒* official⁠⁠⁠⁠ WhatsApp Channel\nHelp us grow and reject that piece of shiet.\n\n';
+    let d = 'https://whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/848e7f1f2b56721405cfc.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
